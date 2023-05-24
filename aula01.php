95848aa5e6e29d818a21c4e761fc10de686ad965
Adrianoespanho <112988600+Adrianoespanho@users.noreply.github.com>
2023-05-24 20:48:01 -0300

    kkkk

---
+++ aula01.php
@@ -0,0 +1,16 @@
+<?php
+//sintaxe e variaveis
+# comentarios
+/*comentario
+de varias linhas */
+
+echo "OLA";
+echo "<h1>php - estudando</h1>";
+print("<h3>aula 02</h2>");
+print_r("vamos estudar!!!");
+
+
+echo "<h3>messagem</h3>";
+echo 9.99;
+
+?>
