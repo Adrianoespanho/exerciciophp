fc5caef055396c563b31db8ab9f141cf158bb4b3
Adrianoespanho <112988600+Adrianoespanho@users.noreply.github.com>
2023-05-24 21:20:20 -0300

    material php
    como usar as variaveis  e quando for numerico 24/05

--- aula01.php
+++ aula01.php
@@ -13,4 +13,41 @@ print_r("vamos estudar!!!");
 echo "<h3>messagem</h3>";
 echo 9.99;
 
+echo "<br>";
+
+// variaveis
+$nome = "Adriano"; //texto =
+$idade = 37; //numerico inteiro =
+$altura = 1.69; // numerico decimal =
+$fumante = false; // booleano =
+
+ echo gettype($nome)  . "<br>";
+ echo gettype($idade) . "<br>";
+ echo gettype($altura) . "<br>";
+ echo gettype($fumante) . "<br>";
+ 
+ echo $nome;
+ echo "<br>";
+ echo $nome . $idade;
+ echo "<br>";
+ echo $nome . " . " . $idade . "<br";
+ echo "nome " . $nome . "<br>"; 
+ echo "nome: $nome <br>";
+ echo 'nome: $nome <br>';
+
+ echo "<h1>Operações</h1>";
+ $n1 = 10;
+ $n2 = 2;
+ echo $n1+$n2;
+ echo "<br>";
+ echo "$n1 + $n2 = ". $n1+$n2 . " <br>";
+ echo $n1 - $n2 . "<br.";
+ echo $n1 + $n2 . "<br.";
+ echo $n1 / $n2 . "<br.";
+ echo $n1 % $n2 . "<br.";
+ echo $n1 ** $n2 . "<br.";
+
+
+
+
 ?>
